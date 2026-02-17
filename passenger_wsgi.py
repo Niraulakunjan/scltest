a92b2a0f7a286e65d6103fd8d9978c5ebe3de282
Kunjan Niraula <niraula67kunjan@gmail.com>
2026-02-17 22:57:07 +0545

    Refactor passenger_wsgi.py to improve structure and ensure proper environment setup

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,14 @@
-import os
 import sys
+import os
 
-sys.path.insert(0, os.path.dirname(__file__))
+INTERP = "/home/niraulakunjan/virtualenv/scl.sajilocode.com/3.12/bin/python3"
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+
+PROJECT_HOME = "/home/niraulakunjan/scl.sajilocode.com"
+if PROJECT_HOME not in sys.path:
+    sys.path.insert(0, PROJECT_HOME)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schoolms.settings")
 
 from schoolms.wsgi import application
